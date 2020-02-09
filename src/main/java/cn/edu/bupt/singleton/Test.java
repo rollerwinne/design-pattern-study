84b59a9d11e8c94217a1da04f50aaa992a030e96
mmc <502358401@qq.com>
2020-02-09 15:20:28 +0800

    add Singleton Chapter

---
+++ src/main/java/cn/edu/bupt/singleton/Test.java
@@ -0,0 +1,7 @@
+package cn.edu.bupt.singleton;
+
+public class Test {
+    public static void main(String[] args) {
+        Runtime.getRuntime();
+    }
+}
---
+++ src/main/java/cn/edu/bupt/singleton/type8/Singleton.java
@@ -0,0 +1,8 @@
+package cn.edu.bupt.singleton.type8;
+
+/**
+ * 单例模式:枚举
+ */
+public enum Singleton {
+    INSTANCE;
+}
