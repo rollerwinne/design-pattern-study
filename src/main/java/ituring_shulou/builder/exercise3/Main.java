01813452ea69168542a699dacc0ce527b3550432
zc260618 <zc260618@alibaba-inc.com>
2020-07-17 17:06:50 +0800

    add Builder Chapter

---
+++ src/main/java/ituring_shulou/builder/exercise3/Main.java
@@ -0,0 +1,13 @@
+package ituring_shulou.builder.exercise3;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        FrameBuilder framebuilder = new FrameBuilder();
+        Director director = new Director(framebuilder);
+        director.construct();
+        JFrame frame = framebuilder.getResult();
+        frame.setVisible(true);
+    }
+}
