d37e89bb3ca34ea21e167508dcfd42df0a4b1786
zc260618 <zc260618@alibaba-inc.com>
2020-07-20 04:34:48 +0800

    add Facade Chapter

---
+++ src/main/java/ituring_shulou/facade/Main.java
@@ -0,0 +1,9 @@
+package ituring_shulou.facade;
+
+import ituring_shulou.facade.pagemaker.PageMaker;
+
+public class Main {
+    public static void main(String[] args) {
+        PageMaker.makeWelcomePage("hyuki@hyuki.com", "welcome.html");
+    }
+}
