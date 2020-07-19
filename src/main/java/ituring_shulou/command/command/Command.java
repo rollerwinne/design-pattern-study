46ba4a5f17dc8399e8365ca49beae44794d77957
zc260618 <zc260618@alibaba-inc.com>
2020-07-20 06:26:17 +0800

    add Command Chapter

---
+++ src/main/java/ituring_shulou/command/command/Command.java
@@ -0,0 +1,5 @@
+package ituring_shulou.command.command;
+
+public interface Command {
+    public abstract void execute();
+}
---
+++ src/main/java/ituring_shulou/command/drawer/Drawable.java
@@ -0,0 +1,5 @@
+package ituring_shulou.command.drawer;
+
+public interface Drawable {
+    public abstract void draw(int x, int y);
+}
